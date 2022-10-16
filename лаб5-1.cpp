309c04c4849cb252a6c1f5e97f8956b9e9d0a485
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-10-16 23:00:18 +0500

    Add files via upload

---
+++ лаб5-1.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+bool Even(int K)
+{
+    cout.setf(ios::boolalpha);
+    bool a;
+    a = ((abs(K) % 2 == 0)&&(K!=0));
+    return a;
+}
+
+int main()
+{
+    int K, kol=0;
+    for (int i = 0;i < 10;i++)
+    {
+        cin >> K;
+        if (Even(K))
+            kol += 1;
+    }
+    cout << kol;
+}
